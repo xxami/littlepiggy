6234d7a41fa393db947937a417d1ffd707ac35df
lilah <lilah.willacott@gmail.com>
2017-01-29 17:08:54 +0000

    use templates for hello world output via html

--- littlepiggy.go
+++ littlepiggy.go
@@ -1,11 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"text/template"
+)
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		w.Write([]byte("Hello, world!"))
+		w.Header().Add("Content Type", "text/html")
+		tmpl, err := template.New("test").Parse(doc)
+		if err == nil {
+			tmpl.Execute(w, nil)
+		}
 	})
 
 	http.ListenAndServe(":8000", nil)
 }
+
+const doc = `
+<!DOCTYPE html>
+<html>
+<head><title></title></head>
+<body>
+    <h1>Hello, world!</h1>
+</body>
+</html>
+`
