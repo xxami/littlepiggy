f01b17a16c8fdd0133435a6db20e8fbb0c01afa4
lilah <lilah.willacott@gmail.com>
2017-01-29 09:22:00 +0000

    add helloworld

---
+++ littlepiggy.go
@@ -0,0 +1,11 @@
+package main
+
+import "net/http"
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		w.Write([]byte("Hello, world!"))
+	})
+
+	http.ListenAndServe(":8000", nil)
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
-# littlepiggy
+## littlepiggy
+![image of a little pig with an ice cream](http://i.imgur.com/QjJjgS0.jpg)
